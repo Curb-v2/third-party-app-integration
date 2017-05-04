1d660d201a33eb0bdc58782ca84e3c002d877afa
Cody Rushing <codyrushing@gmail.com>
2017-05-04 18:43:19 -0500

    begin web app example

---
+++ web-app/public/js/app.js
@@ -0,0 +1,3 @@
+(function(){
+  window.open('https://dev-energycurb.auth0.com/authorize?audience=https://app.dev.energycurb.com&scope=openid offline_access&response_type=code&client_id=yoltvk4XevWSDDF3dhCWMD77lHNAJcH0&redirect_uri=https://app.dev.energycurb.com/curb-code-grant&state=');
+})();
